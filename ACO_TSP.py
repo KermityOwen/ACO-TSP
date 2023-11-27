934bc28b4a92bcc26cb2cf3c3bd0e624098ef33e
Owen Lee <owentheturkey@gmail.com>
2023-11-28 05:46:22 +0800

    Updated

--- ACO_TSP.py
+++ ACO_TSP.py
@@ -3,28 +3,46 @@ import numpy as np
 import xml.dom.minidom as xml
 import random
 from typing import List
+from os.path import isfile
 
 class ACO_TSP:
-    def __init__ (self, graph_path="graph_path", num_ants=58):
-        self.distances = self.parse_graph(graph_path)
-        self.pheramones = self.init_pheramones2(self.distances)
-        self.ants = self.generate_ants(num_ants) # Array of Ants
-        
-        # Heuristic 
-        self.visibilities = 1/self.distances
-        self.visibilities[self.visibilities==np.inf] = 0
-        
-        # print(self.visibility)
-        
+    def __init__ (self, graph_path="graph_path"):
+        """Constructor for ACO_TSP class
+
+        Args:
+            graph_path: path to TSPLIB in XML file. Defaults to "graph_path".
+        """
+        # Parameters
         self.max_epoch = 100
         self.decay_rate = 0.5
+        self.num_ants = 50
         
-        print("distance graph: " + str(self.distances))
+        # Ensure file path is correct
+        if not isfile(graph_path):
+            print("File does not exist in path entered.")
+            print("Please ensure the file name was entered correctly. \nProgram exited.")
+            exit()
+        
+        # Adjacency matrices for distance, pheramones, and our heuristic (visibility)
+        self.distances = self.parse_graph(graph_path)
+        self.pheramones = self.init_pheramones(self.distances)
+        self.visibilities = self.init_visibilities(self.distances) # Our heuristic 
+        
+        # Generates an array of ants
+        # Scatter boolean determines if the ants should all start at 0, or at randomised cities
+        self.ants = self.generate_ants(self.num_ants, scatter=True)
+        
+        # For debug or showcase
+        print("Distances graph: " + str(self.distances))
         print("")
-        print("pheramones graph: " + str(self.pheramones))
+        print("Pheramones graph: " + str(self.pheramones))
         print("")
-        for ant in self.ants:
-            print(ant)
+        print("Heuristics graph: " + str(self.visibilities))
+        print("")
+        
+        # print(str(self.ants[0]) + " x", str(self.num_ants))
+        # for ant in self.ants:
+        #     print(ant)
         
 
     def parse_graph(self, path):
@@ -36,14 +54,15 @@ class ACO_TSP:
         Returns:
             adj_matrix: NumPy adjacency matrix representing the graph
         """
+        # Parse xml into readable data
         xml_data = xml.parse(path)
         all_vertex = xml_data.getElementsByTagName("vertex")
         
         # A nVertex x nVertex sized array filled with 0
         adj_matrix = np.zeros(shape=(len(all_vertex), len(all_vertex)))
         
-        row_index = 0
         # Nested loop to go through every edge
+        row_index = 0
         for vertex in all_vertex:
             tempVertex = vertex.getElementsByTagName("edge")
             for edge in tempVertex:
@@ -56,9 +75,9 @@ class ACO_TSP:
         
         return adj_matrix
         
-
+  
     def init_pheramones(self, graph):
-        """ Initialise pheramones by duping graph matrix and downscaling it 
+        """ Initialise pheramones by generating and filling a nVertex x nVertex sized matrix by 1s
 
         Args:
             graph: Adjacency matrix representing graph distance
@@ -66,38 +85,56 @@ class ACO_TSP:
         Returns:
             pher_matrix: Adjacency matrix representing graph pheramones
         """
-        # A nVertex x nVertex sized array filled with 0
-        pher_matrix = np.zeros(shape=(len(graph), len(graph)))
-        downscale_constant = 0.002
-        
-        # Nested loop to go through every element on the matrix
-        for i in range(0, len(graph)):
-            for j in range(0, len(graph)):
-                # Duping and downscaling distance graph as pheramone graph
-                pher_matrix[i][j] = graph[i][j]*downscale_constant
-                
-        return pher_matrix
-  
-  
-    def init_pheramones2(self, graph):
         return np.ones(shape=(len(graph), len(graph)))
+    
+    def init_visibilities(self, graph):
+        """ Initialise visibility by generating a scaled down distance graph by an exponent of -1
+
+        Args:
+            graph: Adjacency matrix representing graph distance
+
+        Returns:
+            visibilities: Adjacency matrix representing graph visibility
+        """
+        # Visibility is distance scaled down exponentially (pow of -1)
+        visibilities = 1/graph
+        # Ensures if distance is 0, the visibility is also 0 and not 1/0 which is approxed to infinity
+        visibilities[visibilities==np.inf] = 0
+        return visibilities
         
   
-    def generate_ants(self, num_ants) -> List[Ants]:
+    def generate_ants(self, num_ants, scatter=False) -> List[Ants]:
+        """ Generate an array of Ants
+
+        Args:
+            num_ants: Number of ants
+            scatter: Whether or not to scatter the ants position. Defaults to False.
+
+        Returns:
+            arr_ant: Array of Ants
+        """
         arr_ant = []
         for n in range(0, num_ants):
-            # ant_start_pos = random.randint(0, len(self.distances))
-            ant_start_pos = 0
+            if(scatter):
+                # If scatter is True, randomise the starting position for the ants
+                ant_start_pos = random.randint(0, len(self.distances)) 
+            else:
+                # If not all the starting position for all the ants are 0
+                ant_start_pos = 0
             arr_ant.append(Ants(starting_position=ant_start_pos))
         return arr_ant
 
 
     def step_all(self):
+        """ Step all ants forward and calculates their found paths
+        """
         for ant in self.ants:
             ant.find_path(self.pheramones, self.visibilities)
 
 
     def update_pheramones(self):
+        """ Calculates and updates pheramones based on evaluation carried out in Ants class
+        """
         for ant in self.ants:
             # print(ant.eval_cost(self.distances))
             self.pheramones = ant.eval_pher_update(1, self.pheramones, self.distances)
@@ -105,37 +142,44 @@ class ACO_TSP:
 
 
     def decay_pheramones(self):
+        """ Decays / evaporates pheramones by decay rate
+        """
         self.pheramones = self.decay_rate * self.pheramones
         
         
     def epoch(self):
+        """ Run a single iteration / epoch of the ACO for TSP
+        """
         self.step_all()
         self.update_pheramones()
         self.decay_pheramones()
 
 
     def eval(self):
-        self.ants[0].reset_ant()
-        self.ants[0].find_path(self.pheramones, self.visibilities)
-        print(self.ants[0].found_path)
-        print(self.ants[0].eval_cost(self.distances))
-        return self.ants[0].found_path
+        """ Generates an ant and let it find a path based on the pheramones and heuristic for evaluation
+
+        Returns:
+            found_path: Array representing the found path
+            eval_cost: Number representing the total cost (distance) of the found path
+        """
+        evaluator_ant = Ants(0)
+        evaluator_ant.find_path(self.pheramones, self.visibilities)
+        return evaluator_ant.found_path, evaluator_ant.eval_cost(self.distances)
         
     
     def run(self):
-        # print("lel")
+        """ Run max_epoch number of iterations of the ACO
+        """
         for n in range(0, self.max_epoch):
             self.epoch()
-        self.eval()
-        # print("pheramones graph: " + str(self.pheramones))
+        found_path, eval_cost = self.eval()
+        print("Final Path: " + str(found_path))
+        print("Total Distance:" + str(eval_cost))
         
         
 if __name__ == "__main__":
     random.seed(1)
     np.random.seed(1)
-    ACO = ACO_TSP(graph_path="./brazil58.xml")
+    ACO = ACO_TSP(graph_path="./TSPLIB_XML/brazil58.xml")
     ACO.run()
 
-    
-### PROBLEMS WITH CODE RN:
-### PHEROMONES ARE UPDATED AND DECAY TOO FAST
--- Ants.py
+++ Ants.py
@@ -3,27 +3,44 @@ import xml.dom.minidom as xml
 
 class Ants:
     def __init__(self, starting_position=0):
+        """ Construtor for Ants
+
+        Args:
+            starting_position: Ants starting position. Defaults to 0.
+        """
+        # Start position saved for ant resetting
         self.start_pos = starting_position
         self.current_pos = starting_position
         self.found_path = []
 
+
     def find_path(self, pheramones, visibility):
+        """ Finds path for the ant based on pheramones and visibility
+
+        Args:
+            pheramones: Adjacency matrix representing graph pheramones
+            visibility: Adjacency matrix representing graph visibility
+        """
         # Dupe visibility graph to manipulate without affecting other ants during the same epoch
         temp_vis = np.array(visibility)
         
-        # Loop for number of nodes times
+        # Loop for number of cities times
         for n in range(0, len(temp_vis)-1):
-            # Set current pheramone to 0 for no chance of revisiting nodes
+            # Set current column of visibilities to 0 to eliminate revisiting nodes
             temp_vis[:, self.current_pos] = 0
             
-            # All possible path and their pheramones and visibility to travel to from current node
+            # All possible path to travel to from current node and their pheramones
             # Duped to avoid deadlocks
             pher_paths = np.array(pheramones[self.current_pos])
+            
+            # Heuristic to make higher pheramones exponentially more attractive
             pher_paths = np.power(pher_paths, 2)  
             
+            # All possible path to travel to from current node and their visibilities 
             vis_paths = np.array(temp_vis[self.current_pos])
             vis_paths = np.power(vis_paths, 1)  
             
+            # Combining both visibilities and phermones by multiplying each pher by
             combined_paths = np.multiply(pher_paths,vis_paths) 
             
             # Finding the cumulative probability of each node
@@ -31,10 +48,10 @@ class Ants:
             prob_paths = combined_paths/total
             prob_paths = np.cumsum(prob_paths)
             
-            # Finding the next node to travel to
+            # RNG for finding the next node to travel to
             rand = np.random.random_sample()
             
-            # Getting 0,0 of the non zero list eliminates the chances of choosing past nodes
+            # Getting 0,0 of the non zero list eliminates the chances of choosing past visited nodes
             next_node = np.nonzero(prob_paths>rand)[0][0]
             
             # print(possible_paths)
@@ -46,13 +63,20 @@ class Ants:
             self.found_path.append(next_node)
             self.current_pos = next_node
             
+        # Last node to travel to complete cycle
         self.found_path.append(self.start_pos)
         self.current_pos = self.start_pos
         
-        # print(self.found_path)
-        
     
     def eval_cost(self, distances):
+        """ Evaluate total cost of the stored path from ant
+
+        Args:
+            distances: Adjacency matrix representing graph distances
+
+        Returns:
+            total_distance: Total cost of stored path
+        """
         total_distance = 0
         prev_node = self.start_pos
         for n in self.found_path:
@@ -62,47 +86,69 @@ class Ants:
 
 
     def eval_pher_update(self, Q, pheramones, distances):
+        """ Evaluate pheramones for update based on stored path
+
+        Args:
+            Q: Rate of pheramone depositing
+            pheramones: Adjacency matrix representing graph pheramones
+            distances: Adjacency matrix representing graph distances
+
+        Returns:
+            temp_pher: Returns updated adjacency matrix of pheramones
+        """
+        # Dupes pheramones graph to avoid deadlock
         temp_pher = np.array(pheramones)
-        delta_distance = Q/self.eval_cost(distances)
+        
+        # Pheramone to drop off along path
+        pher_dropoff = Q/self.eval_cost(distances)
+        
+        # Loop through the found path 
         prev_node = self.start_pos
         for n in self.found_path:
             # Mirrored for data consistency
-            temp_pher[prev_node][n] = temp_pher[prev_node][n] + delta_distance
-            temp_pher[n][prev_node] = temp_pher[n][prev_node] + delta_distance
-            # print(temp_pher[prev_node,n])
-            
+            temp_pher[prev_node][n] = temp_pher[prev_node][n] + pher_dropoff
+            temp_pher[n][prev_node] = temp_pher[n][prev_node] + pher_dropoff
+
             prev_node = n
         
         return temp_pher
                 
                 
     def reset_ant(self):
+        """ Resets Ant
+        """
         self.current_pos = self.start_pos
         self.found_path = []
         
         
     def __str__(self):
-        return "ant: [-_-], current pos: " + str(self.current_pos)
+        """ To String
+
+        Returns:
+            string: Cute ant descriptor
+        """
+        return "Ant: [-_-], Current Pos: " + str(self.current_pos)
 
 
-if __name__ == "__main__":
-    xml_data = xml.parse("brazil58.xml")
-    all_vertex = xml_data.getElementsByTagName("vertex")
+# DEBUG SHIT
+# if __name__ == "__main__":
+#     xml_data = xml.parse("brazil58.xml")
+#     all_vertex = xml_data.getElementsByTagName("vertex")
         
-    # A nVertex x nVertex sized array filled with 0
-    adj_matrix = np.zeros(shape=(len(all_vertex), len(all_vertex)))
+#     # A nVertex x nVertex sized array filled with 0
+#     adj_matrix = np.zeros(shape=(len(all_vertex), len(all_vertex)))
         
-    row_index = 0
-    # Nested loop to go through every edge
-    for vertex in all_vertex:
-        tempVertex = vertex.getElementsByTagName("edge")
-        for edge in tempVertex:
-            # Gets the content in edge which happens to be the column index
-            column_index = int(edge.childNodes[0].nodeValue)
-            cost = edge.getAttribute("cost")
-            # Assigns the cost to the adjacency matrix
-            adj_matrix[column_index, row_index] = cost
-        row_index += 1
+#     row_index = 0
+#     # Nested loop to go through every edge
+#     for vertex in all_vertex:
+#         tempVertex = vertex.getElementsByTagName("edge")
+#         for edge in tempVertex:
+#             # Gets the content in edge which happens to be the column index
+#             column_index = int(edge.childNodes[0].nodeValue)
+#             cost = edge.getAttribute("cost")
+#             # Assigns the cost to the adjacency matrix
+#             adj_matrix[column_index, row_index] = cost
+#         row_index += 1
         
-    ant = Ants(0)
-    print(ant.eval_cost(adj_matrix))
+#     ant = Ants(0)
+#     print(ant.eval_cost(adj_matrix))
